4aa653e4e2da328c71761866355b23c929acbbb9
Sonve <1051399604@qq.com>
2024-04-11 17:31:00 +0800

    init

--- sv-client/pages/feedback/feedback-list.vue
+++ sv-client/pages/feedback/feedback-list.vue
@@ -10,12 +10,57 @@
       >
         <template #default="{ data }">
           <!-- 列表 -->
-          <view class="feedback-item flex-vc" :class="{ 'border-top': data.index == 0 }">
-            <view class="flex-sub">{{ data.item.feedback_title }}</view>
-            <view class="cuIcon-right text-xl text-gray padding-lr-sm"></view>
+          <view
+            class="feedback-item flex-vc"
+            :class="[data.index == 0 ? 'solid-top' : '']"
+            @click="onFeedback(data.item)"
+          >
+            <view class="feedback-item-box">
+              <view class="flex-vc">
+                <view class="flex-sub text-line-1 text-bold">
+                  {{ data.item.feedback_title }}
+                </view>
+                <uv-tags
+                  size="mini"
+                  :text="statusMap[data.item.feedback_status].text"
+                  :type="statusMap[data.item.feedback_status].type"
+                ></uv-tags>
+              </view>
+              <view class="margin-top-xs text-sm text-line-2">
+                {{ data.item.feedback_content }}
+              </view>
+            </view>
+            <view class="cuIcon-right text-xl text-gray padding-lr"></view>
           </view>
         </template>
       </sv-paging>
+      <uv-popup ref="feedbackDetailRef" mode="center" closeable>
+        <view class="feedback-detail-popup">
+          <view class="popup-header">
+            <view class="popup-title text-line-1">
+              {{ curFeedback.feedback_title }}
+            </view>
+          </view>
+          <view class="popup-content">
+            <view class="popup-detail">
+              <!-- 内容详情 -->
+              <view class="flex-vc">
+                状态：
+                <uv-tags
+                  size="mini"
+                  :text="statusMap[curFeedback.feedback_status].text"
+                  :type="statusMap[curFeedback.feedback_status].type"
+                ></uv-tags>
+              </view>
+              <view class="margin-top-sm">反馈内容：{{ curFeedback.feedback_content }}</view>
+              <view class="margin-top-sm">
+                <view class="margin-bottom-sm">反馈图片：</view>
+                <uv-album keyName="url" :urls="curFeedback.feedback_image"></uv-album>
+              </view>
+            </view>
+          </view>
+        </view>
+      </uv-popup>
     </view>
   </sv-page>
 </template>
@@ -27,12 +72,27 @@ import { feedbackList } from '@/service/api/sys'
 
 const dataParams = ref({ user_id: store.userInfo._id })
 
+const statusMap = {
+  0: { text: '审批中', type: 'primary' },
+  1: { text: '已解决', type: 'success' },
+  2: { text: '已拒绝', type: 'error' }
+}
+
 function queryRes(e) {
   console.log('==== queryRes :', e)
 }
 
 function refreshOK(e) {
   console.log('==== refreshOK :', e)
+}
+
+const feedbackDetailRef = ref()
+const curFeedback = ref()
+
+function onFeedback(e) {
+  console.log('==== onFeddback :', e)
+  curFeedback.value = e
+  feedbackDetailRef.value.open()
 }
 </script>
 
@@ -41,17 +101,51 @@ function refreshOK(e) {
   height: var(--page-notab-height);
 
   .feedback-item {
-    height: 100rpx;
-    border-bottom: 1px solid #cccccc;
-
     @include useTheme {
+      border-bottom: 1px solid #{getTheme('sv-border-color')};
+      background-color: getTheme('sv-card-color');
       &:active {
         background-color: getTheme('sv-hover-color');
       }
     }
+
+    .feedback-item-box {
+      flex: 1;
+      padding: 20rpx 0 20rpx 30rpx;
+    }
   }
-  .border-top {
-    border-top: 1px solid #cccccc;
+  .solid-top {
+    @include useTheme {
+      border-top: 1px solid #{getTheme('sv-border-color')};
+    }
+  }
+
+  .feedback-detail-popup {
+    width: 80vw;
+    height: 80vh;
+
+    @include useTheme {
+      background-color: getTheme('sv-card-color');
+    }
+
+    .popup-header {
+      height: 96rpx;
+      padding: 30rpx 96rpx 30rpx 30rpx;
+
+      .popup-title {
+        font-weight: 700;
+      }
+    }
+
+    .popup-content {
+      padding: 0 30rpx 30rpx 30rpx;
+      height: calc(100% - 96rpx);
+
+      .popup-detail {
+        height: 100%;
+        overflow: auto;
+      }
+    }
   }
 }
 </style>
---
+++ sv-service/uni_modules/sv-system/uniCloud/cloudfunctions/sv-api-sys/module/feedback/index.js
@@ -0,0 +1,148 @@
+const handler = require('sv-handler')
+const db = uniCloud.database()
+const dbCmd = db.command
+
+module.exports = {
+  // 反馈列表
+  async feedbackList() {
+    let {
+      user_id,
+      feedback_id,
+      feedback_title,
+      feedback_type,
+      pagesize = 20,
+      pagenum = 1
+    } = this.params
+
+    // 转换为Number类型
+    pagesize = +pagesize
+    pagenum = +pagenum
+
+    // 页码不可小于1
+    if (pagenum < 1) {
+      throw handler.result({
+        code: 40001,
+        message: 'pagenum不可小于1'
+      })
+    }
+
+    let feedbackRes, pages, total
+
+    const dbJQL = uniCloud.databaseForJQL({
+      clientInfo: this.getClientInfo()
+    })
+
+    // 连接表实例
+    let query = dbJQL.collection('sv-sys-feedback')
+
+    // 构建筛选条件(对象形式)
+    const conditions = {}
+    if (user_id) {
+      conditions.user_id = dbCmd.eq(user_id)
+    }
+    if (feedback_id) {
+      conditions.feedback_id = dbCmd.eq(feedback_id)
+    }
+    if (feedback_title) {
+      conditions.feedback_title = dbCmd.eq(feedback_title)
+    }
+    if (feedback_type) {
+      conditions.feedback_type = dbCmd.eq(Number(feedback_type))
+    }
+
+    // 将所有有效的筛选条件添加到查询对象中
+    if (Object.keys(conditions).length > 0) {
+      query = query.where(conditions).orderBy('create_date', 'desc')
+    }
+
+    if (pagesize < 1) {
+      // 全量查询
+      feedbackRes = await query.get({
+        getCount: true
+      })
+      total = feedbackRes.count
+      pages = 1
+    } else {
+      // 分页查询
+      feedbackRes = await query.skip(pagesize * (pagenum - 1)).limit(pagesize).get({
+        getCount: true
+      })
+      total = feedbackRes.count
+      pages = Math.ceil(total / pagesize)
+    }
+
+    return handler.result({
+      data: feedbackRes.data,
+      total,
+      pagesize,
+      pagenum,
+      pages,
+      params: this.params
+    })
+  },
+
+  // 反馈添加
+  async feedbackAdd() {
+    const {
+      user_id,
+      feedback_id,
+      feedback_content
+    } = this.params
+
+    if (!user_id || !feedback_id || !feedback_content) {
+      throw handler.result({
+        code: 40001
+      })
+    }
+
+    const feedbackRes = await db.collection('sv-sys-feedback').add(this.params)
+
+    return handler.result({
+      data: feedbackRes,
+      message: '提交成功'
+    })
+  },
+
+  // 反馈更新
+  async feedbackUpdate() {
+    const {
+      feedback_id
+    } = this.params
+
+    if (!feedback_id) {
+      throw handler.result({
+        code: 40001
+      })
+    }
+
+    delete this.params._id // 移除_id字段，_id不参与更新
+    const feedbackRes = await db.collection('sv-sys-feedback').where({
+      'feedback_id': dbCmd.eq(feedback_id)
+    }).update(this.params)
+
+    return handler.result({
+      data: feedbackRes,
+    })
+  },
+  
+  // 反馈删除
+  async feedbackDelete() {
+    const {
+      feedback_id
+    } = this.params
+  
+    if (!feedback_id) {
+      throw handler.result({
+        code: 40001
+      })
+    }
+  
+    const feedbackRes = await db.collection('sv-sys-feedback').where({
+      'feedback_id': dbCmd.eq(feedback_id)
+    }).remove()
+  
+    return handler.result({
+      data: feedbackRes,
+    })
+  },
+}

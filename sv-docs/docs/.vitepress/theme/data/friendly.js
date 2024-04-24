7722752693db92bf0570bb2ea1986dcef54bf569
Sonve <1051399604@qq.com>
2024-04-24 15:01:16 +0800

    update

--- sv-docs/docs/.vitepress/theme/data/friendly.js
+++ sv-docs/docs/.vitepress/theme/data/friendly.js
@@ -8,9 +8,9 @@ export function useFriendly() {
     if (data.value && data.value.length) return;
 
     // const result = await fetch("https://mp-74bfcbac-6ba6-4f39-8513-8831390ff75a.cdn.bspapp.com/json/friendly.json"); // 会跨域
-    // const json = await result.json(); 
-    data.value = json && json.links ? json.links : [];
+    // const json = await result.json();
 
+    data.value = json && json.links ? json.links : [];
   });
 
   return {
